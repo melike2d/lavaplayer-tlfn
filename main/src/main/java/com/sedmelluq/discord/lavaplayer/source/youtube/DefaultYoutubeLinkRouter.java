748b094b68db06f6ba66e77dcd9a5292fe9e3194
shilu <dayeonkim5831@gmail.com>
2021-08-01 13:32:41 +0900

    Support embed and shorts URLs

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/DefaultYoutubeLinkRouter.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/DefaultYoutubeLinkRouter.java
@@ -1,10 +1,12 @@
 package com.sedmelluq.discord.lavaplayer.source.youtube;
 
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+
 import java.net.URISyntaxException;
 import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URIBuilder;
 
@@ -22,11 +24,13 @@ public class DefaultYoutubeLinkRouter implements YoutubeLinkRouter {
 
   private static final Pattern directVideoIdPattern = Pattern.compile("^" + VIDEO_ID_REGEX + "$");
 
-  private final Extractor[] extractors = new Extractor[] {
-      new Extractor(directVideoIdPattern, Routes::track),
-      new Extractor(Pattern.compile("^" + PLAYLIST_ID_REGEX + "$"), this::routeDirectPlaylist),
-      new Extractor(Pattern.compile("^" + PROTOCOL_REGEX + DOMAIN_REGEX + "/.*"), this::routeFromMainDomain),
-      new Extractor(Pattern.compile("^" + PROTOCOL_REGEX + SHORT_DOMAIN_REGEX + "/.*"), this::routeFromShortDomain)
+  private final Extractor[] extractors = new Extractor[]{
+          new Extractor(directVideoIdPattern, Routes::track),
+          new Extractor(Pattern.compile("^" + PLAYLIST_ID_REGEX + "$"), this::routeDirectPlaylist),
+          new Extractor(Pattern.compile("^" + PROTOCOL_REGEX + DOMAIN_REGEX + "/.*"), this::routeFromMainDomain),
+          new Extractor(Pattern.compile("^" + PROTOCOL_REGEX + SHORT_DOMAIN_REGEX + "/.*"), this::routeFromShortDomain),
+          new Extractor(Pattern.compile("^" + PROTOCOL_REGEX + DOMAIN_REGEX + "/embed/.*"), this::routeFromEmbed),
+          new Extractor(Pattern.compile("^" + PROTOCOL_REGEX + DOMAIN_REGEX + "/shorts/.*"), this::routeFromShorts)
   };
 
   @Override
@@ -105,6 +109,16 @@ public class DefaultYoutubeLinkRouter implements YoutubeLinkRouter {
     return routeFromUrlWithVideoId(routes, urlInfo.path.substring(1), urlInfo);
   }
 
+  protected <T> T routeFromEmbed(Routes<T> routes, String url) {
+    UrlInfo urlInfo = getUrlInfo(url, true);
+    return routeFromUrlWithVideoId(routes, urlInfo.path.substring(7), urlInfo);
+  }
+
+  protected <T> T routeFromShorts(Routes<T> routes, String url) {
+    UrlInfo urlInfo = getUrlInfo(url, true);
+    return routeFromUrlWithVideoId(routes, urlInfo.path.substring(8), urlInfo);
+  }
+
   private static UrlInfo getUrlInfo(String url, boolean retryValidPart) {
     try {
       if (!url.startsWith("http://") && !url.startsWith("https://")) {
@@ -113,8 +127,8 @@ public class DefaultYoutubeLinkRouter implements YoutubeLinkRouter {
 
       URIBuilder builder = new URIBuilder(url);
       return new UrlInfo(builder.getPath(), builder.getQueryParams().stream()
-          .filter(it -> it.getValue() != null)
-          .collect(Collectors.toMap(NameValuePair::getName, NameValuePair::getValue, (a, b) -> a)));
+              .filter(it -> it.getValue() != null)
+              .collect(Collectors.toMap(NameValuePair::getName, NameValuePair::getValue, (a, b) -> a)));
     } catch (URISyntaxException e) {
       if (retryValidPart) {
         return getUrlInfo(url.substring(0, e.getIndex() - 1), false);
