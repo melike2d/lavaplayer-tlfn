859af4d937255103e011cfc8e8b2a6f281379241
melike2d <gino@dimensional.fun>
2021-08-18 22:17:08 -0700

    use java 16

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -63,7 +63,11 @@ publishing {
 repositories {
     mavenCentral()
 }
-
-tasks.withType<KotlinCompile> {
-  kotlinOptions.jvmTarget = "16"
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
